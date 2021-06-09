604d216654c3203eb1a311389339058cba0fb600
Charles Simms <cwsimms@umich.edu>
2021-06-09 17:41:45 -0400

    worker00 test

--- mapreduce/worker/__main__.py
+++ mapreduce/worker/__main__.py
@@ -4,34 +4,130 @@ import json
 import time
 import click
 import mapreduce.utils
+import threading
+import socket
 
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
-
+    
 
 class Worker:
     def __init__(self, master_port, worker_port):
         logging.info("Starting worker:%s", worker_port)
         logging.info("Worker:%s PWD %s", worker_port, os.getcwd())
-
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register_ack",
+        
+        self.alive = False
+        
+        # Get PID
+        pid = os.getpid()
+                
+        listen_thread = threading.Thread(target=self.listeningToMaster, args=(worker_port,))
+        listen_thread.start()
+                
+        # Send register message to Master
+        
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("localhost", master_port))
+        
+        register_message = {
+            "message_type": "register",
             "worker_host": "localhost",
-            "worker_port": 6001,
-            "worker_pid": 77811
+            "worker_port": worker_port,
+            "worker_pid": pid
         }
-        logging.debug(
-            "Worker:%s received\n%s",
-            worker_port,
-            json.dumps(message_dict, indent=2),
-        )
+        
+        message = json.dumps(register_message)
+        sock.sendall(message.encode('utf-8'))
+        sock.close()
+                
+        listen_thread.join() # wait for the listen_thread to return
+        
+        # This is a fake message to demonstrate pretty printing with logging
+        #message_dict = {"message_type": "register_ack", "worker_host": "localhost", "worker_port": 6001, "worker_pid":77811}
+        
+        #logging.debug("Worker:%s received\n%s", worker_port, json.dumps(message_dict, indent=2),)
 
         # TODO: you should remove this. This is just so the program doesn't
         # exit immediately!
-        logging.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        #logging.debug("IMPLEMENT ME!")
+        #time.sleep(120)
+
+
+    def listeningToMaster(self,worker_port):
+        # TCP Socket that listens for instructions
+
+        listen_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the server
+        listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        listen_sock.bind(("localhost", worker_port))
+        listen_sock.listen()
+
+        # Socket accept() and recv() will block for a maximum of 1 second.  If you
+        # omit this, it blocks indefinitely, waiting for a connection.
+        listen_sock.settimeout(1)
+
+        while True:
+            # Wait for a connection for 1s.  The socket library avoids consuming
+            # CPU while waiting for a connection.
+            try:
+                clientsocket, address = listen_sock.accept()
+            except socket.timeout:
+                continue
+            print("Connection from", address[0])
+
+            # Receive data, one chunk at a time.  If recv() times out before we can
+            # read a chunk, then go back to the top of the loop and try again.
+            # When the client closes the connection, recv() returns empty data,
+            # which breaks out of the loop.  We make a simplifying assumption that
+            # the client will always cleanly close the connection.
+            message_chunks = []
+            while True:
+                try:
+                    data = clientsocket.recv(4096)
+                except socket.timeout:
+                    continue
+                if not data:
+                    break
+                message_chunks.append(data)
+            clientsocket.close()
+
+            # Decode list-of-byte-strings to UTF8 and parse JSON data
+            message_bytes = b''.join(message_chunks)
+            message_str = message_bytes.decode("utf-8")
+
+            try:
+                message_dict = json.loads(message_str)
+            except json.JSONDecodeError:
+                continue
+
+            if message_dict['message_type'] == "shutdown":
+                self.alive = False
+                break
+            elif message_dict['register_ack'] == "register_ack": 
+                self.alive = True
+                self.heart_thread = threading.Thread(target=self.heartbeat, args=(master_port,))
+                self.heart_thread.start()
+
+
+    
+    def heartbeat(self,master_port):
+        # Create an INET, DGRAM socket, this is UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+        # Connect to the UDP socket on server
+        sock.connect(("localhost", master_port))
+
+        message = {
+            "message_type": "heartbeat",
+            "worker_pid": self.pid
+        }
+
+        # Send a message
+        message = json.dumps({"hello": "world"})
+        sock.sendall(message.encode('utf-8'))
+        sock.close()
 
 
 @click.command()
