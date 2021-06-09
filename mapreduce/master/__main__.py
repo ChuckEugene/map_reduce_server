d349ad1732cf7c44568322e877950e4ead0c0466
chagrady <chagrady@umich.edu>
2021-06-09 17:48:37 -0400

    master constructor started

--- mapreduce/master/__main__.py
+++ mapreduce/master/__main__.py
@@ -4,6 +4,9 @@ import json
 import time
 import click
 import mapreduce.utils
+import glob
+import threading
+import socket
 
 
 # Configure logging
@@ -15,22 +18,52 @@ class Master:
         logging.info("Starting master:%s", port)
         logging.info("Master:%s PWD %s", port, os.getcwd())
 
-        # This is a fake message to demonstrate pretty printing with logging
-        message_dict = {
-            "message_type": "register",
-            "worker_host": "localhost",
-            "worker_port": 6001,
-            "worker_pid": 77811
-        }
-        logging.debug("Master:%s received\n%s",
-            port,
-            json.dumps(message_dict, indent=2),
-        )
+        #set master to active
+        self.active = True
 
-        # TODO: you should remove this. This is just so the program doesn't
-        # exit immediately!
-        logging.debug("IMPLEMENT ME!")
-        time.sleep(120)
+        #initialize port 
+        self.port = port
+
+        #create new directory and delete old files
+        Path('tmp').mkdir(exist_ok=True)
+        for i in glob.glob('tmp/job-*')
+            os.remove(i)
+
+        #initialize thread container
+        self.threads = []
+
+        #create UDP listener and fault tolerance thread
+        listener = threading.Thread(target=self.listen_for_UDP)
+        fault_tol = threading.Thread(target=self.fault_tolerance)
+
+        self.threads.append(listener)
+        self.threads.append(fault_tol)
+
+        #the threads are run 
+        for thread in threads:
+            thread.start()
+
+
+    def listen_for_UDP(self):
+        #the socket that takes in jobs is created
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("localhost", self.port - 1))
+
+        sock.settimeout(1)
+
+
+
+        try:
+            msg = json.loads(msg)
+        except JSONDecodeError:
+            continue
+
+    def fault_tolerance(self):
+
+
+
+
 
 
 @click.command()
