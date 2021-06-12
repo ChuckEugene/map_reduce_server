65bdedc047bce696c32b69bb981f79285deba6fa
Charles Simms <cwsimms@umich.edu>
2021-06-12 18:06:28 -0400

    only integration and master5

--- mapreduce/master/__main__.py
+++ mapreduce/master/__main__.py
@@ -9,6 +9,8 @@ import threading
 import shutil
 import socket
 from pathlib import Path
+import heapq
+from contextlib import ExitStack
 
 
 # Configure logging
@@ -22,7 +24,6 @@ class Master:
 
         #set master to active
         self.active = True
-        self.active_job = False
 
         #initialize port
         self.port = port
@@ -40,23 +41,26 @@ class Master:
 
         #Start job id at 0
         self.jobid = 0
-
-        self.currentjobid = -1
+        
         self.activejob = False
         self.currentTask = []
+        self.sortedNum = 1
+        self.currentWorkers = 0
+        self.worker_reg_order = []
 
         #create UDP listener and fault tolerance thread
         UDP_listener = threading.Thread(target=self.listen_UDP)
         TCP_listener = threading.Thread(target=self.listen_TCP)
-
+        
         self.threads.append(UDP_listener)
         self.threads.append(TCP_listener)
 
         #the threads are run
         for thread in self.threads:
             thread.start()
-
-
+            
+                
+        
     def listen_UDP(self):
         #the socket that takes in worker pings
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -77,11 +81,6 @@ class Master:
 
             try:
                 message_dict = json.loads(message_str)
-                logging.debug(
-                    "Master:%s received\n%s",
-                    self.port,
-                    json.dumps(message_dict, indent=2),
-                )
             except JSONDecodeError:
                 continue
         sock.close()
@@ -120,11 +119,6 @@ class Master:
 
             try:
                 message_dict = json.loads(message_str)
-                logging.debug(
-                    "Master:%s received\n%s",
-                    self.port,
-                    json.dumps(message_dict, indent=2),
-                )
             except json.JSONDecodeError:
                 continue
 
@@ -133,77 +127,253 @@ class Master:
 
     def handle_message(self, message):
         msg_type = message['message_type']
-
+        
         if msg_type == 'shutdown':
             self.shutdown()
 
         if msg_type == 'register':
             self.register(message)
-            if len(self.workers) == 1:
-                self.check_jobs()
+            #if len(self.workers) == 1:
+            self.check_jobs()
 
         if msg_type == 'new_master_job':
             self.new_master_job(message)
-
+            
         if msg_type == 'status':
-            self.workers[message['worker_pid']]['status'] = 'ready'
-            self.check_jobs()
+            
+            if self.workers[message['worker_pid']]['status'] != "ready":
+                self.currentWorkers = self.currentWorkers - 1
+                self.workers[message['worker_pid']]['status'] = "ready"
+                self.check_jobs()
 
 
-    def check_jobs(self):
+    def check_jobs(self):        
         if self.activejob:
             self.run_job(self.jobs[0])
             return
         elif len(self.jobs) == 0 or len(self.workers) == 0:
             return
         else: #we have jobs and workers available and a job isn't currently running
-            self.currentjobid = self.currentjobid + 1
             self.run_job(self.jobs[0])
             return
 
     def run_job(self, jobDict):
         self.activejob = True
         #Run the job
+                
+        if len(self.currentTask) == 0 and self.currentWorkers == 0 and jobDict['status'] == "map":
+            logging.info("Master:%s end map stage", self.port)
+            jobDict['status'] = "Startgroup"
+        elif len(self.currentTask) == 0 and self.currentWorkers == 0 and jobDict['status'] == "group":
+            self.sortedNum = 1
+            logging.info("Master:%s end group stage", self.port)
+            jobDict['status'] = "Startreduce"
 
-        if jobDict['status'] == "map":
+            grouperOut = os.listdir("tmp/job-"+str(jobDict['job_id'])+"/grouper-output")
 
-            if len(self.currentTask) == 0:
+            files = [open("tmp/job-"+str(jobDict['job_id'])+"/grouper-output/" +fn) for fn in grouperOut]
+
+            with ExitStack() as stack:
+                red_files = [stack.enter_context(open("tmp/job-"+str(jobDict['job_id'])+"/grouper-output/reduce" +"{:02d}".format(i+1), 'w'))  for i in range(jobDict['message']['num_reducers'])]
+
+                prevLine = None
+
+                uniq_keys = -1
+
+                for line in heapq.merge(*files):
+
+                    if line != prevLine:
+                        uniq_keys = uniq_keys + 1
+                        prevLine = line
+
+                    red_files[uniq_keys % jobDict['message']['num_reducers']].write(line)
+                    
+        elif len(self.currentTask) == 0 and self.currentWorkers == 0 and jobDict['status'] == "reduce":
+            logging.info("Master:%s end reduce stage", self.port)
+            jobDict['status'] = "done"
+
+            input_files = os.listdir("tmp/job-"+str(jobDict['job_id'])+"/reducer-output")
+
+            (Path(jobDict['message']['output_directory'])).mkdir(exist_ok=True)
+
+            for i, file in enumerate(input_files):
+                shutil.copy("tmp/job-"+str(jobDict['job_id'])+"/reducer-output/" + file, jobDict['message']['output_directory'])
+
+                os.rename(jobDict['message']['output_directory'] + "/" + file , jobDict['message']['output_directory'] + "/outputfile" + "{:02d}".format(i+1)) 
+
+            jobDict['status'] = "done"
+            self.jobs.pop(0)
+            self.activejob = False
+            self.check_jobs()
+        
+        if jobDict['status'] == "StartMap" or jobDict['status'] == "map":
+            if len(self.currentTask) == 0 and jobDict['status'] == "StartMap": #self.currentTask initialized
                 logging.info("Master:%s begin map stage", self.port)
-
+                
                 input_files = os.listdir(jobDict['message']['input_directory'])
                 input_files.sort()
-
-                self.currentTask = [[jobDict['message']['input_directory'] + "/" + input_files[z] for z in range(y, len(input_files), jobDict['message']['num_mappers'])]
+                
+                self.currentTask = [[jobDict['message']['input_directory'] + "/" + input_files[z] for z in range(y, len(input_files), jobDict['message']['num_mappers'])] 
                                             for y in range(jobDict['message']['num_mappers'])]
-
-            for worker in self.workers.values():
+                
+                jobDict['status'] = "map"
+            
+            for pid in self.worker_reg_order:
+                worker = self.workers[pid]
                 if worker['status'] == "ready":
-                    message = {
-                      "message_type": "new_worker_task",
-                      "input_files": self.currentTask[0],
-                      "executable": jobDict['message']['mapper_executable'],
-                      "output_directory": "tmp/job-"+str(jobDict['job_id'])+"/mapper-output",
-                      "worker_pid": worker['worker_pid']
-                    }
+                    if len(self.currentTask) != 0:
+                                                
+                        message = {
+                          "message_type": "new_worker_task",
+                          "input_files": self.currentTask[0],
+                          "executable": jobDict['message']['mapper_executable'],
+                          "output_directory": "tmp/job-"+str(jobDict['job_id'])+"/mapper-output",
+                          "worker_pid": worker['worker_pid']
+                        }
 
-                    self.currentTask.pop(0)
+                        self.currentTask.pop(0)
+                        self.currentWorkers = self.currentWorkers + 1
 
-                    self.send_message(worker['worker_pid'],message)
+                        self.send_message(worker['worker_pid'],message)
 
-                    worker['status'] = 'busy'
-
-            if len(self.currentTask) == 0:
+                        self.workers[worker['worker_pid']]['status'] = 'busy1'
+                                    
+            if len(self.currentTask) == 0 and self.currentWorkers == 0: 
                 logging.info("Master:%s end map stage", self.port)
+                jobDict['status'] = "Startgroup"
+            
+        elif jobDict['status'] == "Startgroup" or jobDict['status'] == "group":
+            
+            if jobDict['status'] == "Startgroup":
+                logging.info("Master:%s begin group stage", self.port)
+
+                input_files = os.listdir("tmp/job-"+str(jobDict['job_id'])+"/mapper-output")
+                input_files.sort()
+
+                count = 0
+
+                #workers available
+                for worker in self.workers.values():
+                    if worker['status'] == "ready":
+                        count = count + 1
+
+                if count > len(input_files):
+                    count = len(input_files)
+
+                self.currentTask = [["tmp/job-"+str(jobDict['job_id'])+"/mapper-output" + "/" + input_files[z] for z in range(y, len(input_files), count)] 
+                                                for y in range(count)]
+                
                 jobDict['status'] = "group"
+            
+            
+            for pid in self.worker_reg_order:
+                worker = self.workers[pid]
+                
+                if worker['status'] == "ready":
+                    if len(self.currentTask) != 0:
+                        
+                        sortNum = str(self.sortedNum)
+                        
+                        if self.sortedNum < 10:
+                            sortNum = "0" + sortNum
+                        
+                        message = {
+                          "message_type": "new_sort_task",
+                          "input_files": self.currentTask[0],
+                          "output_file": "tmp/job-"+str(jobDict['job_id'])+"/grouper-output/sorted" + sortNum,
+                          "worker_pid": worker['worker_pid']
+                        }
+                        
+                        self.sortedNum = self.sortedNum + 1
 
-        elif jobDict['status'] == "group":
-            print("Group")
-        elif jobDict['status'] == "reduce":
-            print("Reduce")
+                        self.currentTask.pop(0)
+                        self.currentWorkers = self.currentWorkers + 1
 
+                        self.send_message(worker['worker_pid'],message)
+                        
+                        self.workers[worker['worker_pid']]['status'] = 'busy2'
+                        
+            
+            if len(self.currentTask) == 0 and self.currentWorkers == 0:
+                self.sortedNum = 1
+                logging.info("Master:%s end group stage", self.port)
+                jobDict['status'] = "Startreduce"
+                                
+                grouperOut = os.listdir("tmp/job-"+str(jobDict['job_id'])+"/grouper-output")
+                                
+                files = [open("tmp/job-"+str(jobDict['job_id'])+"/grouper-output/" +fn) for fn in grouperOut]
+                                                                
+                with ExitStack() as stack:
+                    red_files = [stack.enter_context(open("tmp/job-"+str(jobDict['job_id'])+"/grouper-output/reduce" +"{:02d}".format(i+1), 'w'))  for i in range(jobDict['message']['num_reducers'])]
+                
+                    prevLine = None
+                    
+                    uniq_keys = -1
 
+                    for line in heapq.merge(*files):
+                        
+                        if line != prevLine:
+                            uniq_keys = uniq_keys + 1
+                            prevLine = line
+                                                                            
+                        red_files[uniq_keys % jobDict['message']['num_reducers']].write(line)
+            
+                            
+        elif jobDict['status'] == "Startreduce" or jobDict['status'] == "reduce":
+            if jobDict['status'] == "Startreduce":
+                logging.info("Master:%s begin reduce stage", self.port)
+                
+                input_files = os.listdir("tmp/job-"+str(jobDict['job_id'])+"/grouper-output")
+                input_files.sort()
+                                
+                self.currentTask = [["tmp/job-"+str(jobDict['job_id'])+"/grouper-output/reduce" + "{:02d}".format(z+1) for z in range(jobDict['message']['num_reducers'])]
+                                            for y in range(jobDict['message']['num_reducers'])]
+                
+                jobDict['status'] = "reduce"
+            
+            for pid in self.worker_reg_order:
+                worker = self.workers[pid]
+                
+                if worker['status'] == "ready":
+                    if len(self.currentTask) != 0:
+                        message = {
+                          "message_type": "new_worker_task",
+                          "input_files": self.currentTask[0],
+                          "executable": jobDict['message']['reducer_executable'],
+                          "output_directory": "tmp/job-"+str(jobDict['job_id'])+"/reducer-output",
+                          "worker_pid": worker['worker_pid']
+                        }
+                        
 
+                        self.currentTask.pop(0)
+                        self.currentWorkers = self.currentWorkers + 1
+                        
 
+                        self.send_message(worker['worker_pid'],message)
+
+                        self.workers[worker['worker_pid']]['status'] = 'busy3'
+                        
+            
+            if len(self.currentTask) == 0 and self.currentWorkers == 0:
+                logging.info("Master:%s end reduce stage", self.port)
+                jobDict['status'] = "done"
+                
+                input_files = os.listdir("tmp/job-"+str(jobDict['job_id'])+"/reducer-output")
+                            
+                (Path(jobDict['message']['output_directory'])).mkdir(exist_ok=True)
+
+                for i, file in enumerate(input_files):
+                    shutil.copy("tmp/job-"+str(jobDict['job_id'])+"/reducer-output/" + file, jobDict['message']['output_directory'])
+
+                    os.rename(jobDict['message']['output_directory'] + "/" + file , jobDict['message']['output_directory'] + "/outputfile" + "{:02d}".format(i+1)) 
+                    
+                jobDict['status'] = "done"
+                self.jobs.pop(0)
+                self.activejob = False
+                
+            
+            
+        
 
     def new_master_job(self, message):
         #assign job id and increment counter
@@ -215,7 +385,7 @@ class Master:
         (self.temp_path / idstr / 'grouper-output').mkdir(exist_ok=True)
         (self.temp_path / idstr / 'reducer-output').mkdir(exist_ok=True)
 
-        self.jobs.append({"message": message, "job_id": self.jobid, "status": "map"})
+        self.jobs.append({"message": message, "job_id": self.jobid, "status": "StartMap"})
         #Status: "map" -> "group" -> "reduce"
         self.jobid += 1
         self.check_jobs()
@@ -235,6 +405,8 @@ class Master:
         }
 
         self.workers[w_pid] = worker
+        
+        self.worker_reg_order.append(w_pid)
 
         worker_msg = {
             'message_type': 'register_ack',
